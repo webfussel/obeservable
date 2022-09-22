d59ed795fd133772f644d47a611f6523000aa70e
webfussel <fiona.lena.toma@gmail.com>
2022-09-22 13:10:21 +0200

    ADD: adjustments to build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
-import { terser } from 'rollup-plugin-terser'
+import {terser} from 'rollup-plugin-terser'
 
 export default {
-  input: 'src/index.ts', // our source file
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
